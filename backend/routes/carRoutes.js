2a622eb6414bdfc189a3ba0a3f5f8bf6bfa59c4b
ahmed-a133b <ahmedateeb109@gmail.com>
2025-04-24 21:50:43 +0500

    Modified backend/carRoutes.js

--- backend/routes/carRoutes.js
+++ backend/routes/carRoutes.js
@@ -1,15 +1,22 @@
 const express = require("express")
-const { getCars, getCarById, createCar, updateCar, deleteCar, getFeaturedCars } = require("../controllers/carController")
+const {
+  getCars,
+  getCarById,
+  createCar,
+  updateCar,
+  deleteCar,
+  getFeaturedCars,
+} = require("../controllers/carController")
 const { protect, authorize } = require("../middleware/authMiddleware")
 
 const router = express.Router()
 
 router.get("/", getCars)
+router.get("/featured", getFeaturedCars) // moved this ABOVE `/:id`
 router.get("/:id", getCarById)
-router.get("/featured", getFeaturedCars)
+
 router.post("/", protect, authorize("dealership_manager", "admin"), createCar)
 router.put("/:id", protect, authorize("dealership_manager", "admin"), updateCar)
 router.delete("/:id", protect, authorize("dealership_manager", "admin"), deleteCar)
-
 
 module.exports = router
