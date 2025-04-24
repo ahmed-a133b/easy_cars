33ede115ecd3325e05031151fafa7da8e068e385
ahmed-a133b <ahmedateeb109@gmail.com>
2025-04-24 19:33:05 +0500

    Modified /backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -30,8 +30,12 @@ const PORT = process.env.PORT || 5000
 app.set('trust proxy', 1);
 
 app.use(express.json())
-app.use(cors())
+
 app.use(rateLimiter)
+
+app.use(cors({
+  origin: 'https://easy-cars-enu6ja8rr-ahmed-a133bs-projects.vercel.app/'
+}));
 
 // Routes
 app.use("/api/auth", authRoutes)
